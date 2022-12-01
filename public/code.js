9a02d961ed34b6f48d5b9c1c25ec061c125a7922
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-12-01 09:30:39 +0200

    first chat done

--- public/code.js
+++ public/code.js
@@ -36,7 +36,7 @@
       renderUsers(users);
       console.log(users);
       let usernames = app.querySelector(".join-screen #username").value;
-      //let username = usernames;
+
       console.log(usernames);
       if (usernames === "") {
         return;
@@ -48,6 +48,7 @@
       app.querySelector(".chat-screen").classList.add("active");
       let header = document.querySelector(".chat-screen .header");
       let logged = document.createElement("div");
+      logged.setAttribute("class", "logged");
       header.append(logged);
       logged.innerText = `Logged in as ${usernames}`;
 
@@ -70,13 +71,11 @@
     });
     list.append(...persons);
     initialisationListener(persons);
-    // initialisation();
   }
   function initialisationListener(persons) {
     persons.forEach((personEle) => {
       personEle.addEventListener("click", () => {
         activateChat(personEle);
-        // initialisation();
       });
     });
   }
@@ -142,69 +141,10 @@
     messagesList.append(...messagesUser);
   }
 
-  // app
-  //   .querySelector(".chat-screen #send-message")
-  //   .addEventListener("click", function () {
-  //     let message = app.querySelector(".chat-screen #message-input").value;
-  //     if (message.length === 0) {
-  //       return;
-  //     }
-  //     renderMessage("my", {
-  //       username: uname,
-  //       text: message,
-  //     });
-  //     socket.emit("chat", {
-  //       username: uname,
-  //       text: message,
-  //     });
-  //     app.querySelector(".chat-screen #message-input").value = "";
-  //   });
-
   app
     .querySelector(".chat-screen #exit-chat")
     .addEventListener("click", function () {
       socket.emit("exituser", uname);
       window.location.href = window.location.href;
     });
-
-  // socket.on("update", function (update) {
-  //   renderMessage("update", update);
-  // });
-  // socket.on("chat", function (message) {
-  //   renderMessage("other", message);
-  // });
-
-  //   function renderMessage(type, message) {
-  //     let messageContainer = app.querySelector(".chat-screen .messages");
-  //     if (type === "my") {
-  //       let el = document.createElement("div");
-  //       el.setAttribute("class", "message my-message");
-  //       el.innerHTML = `
-  //       <div>
-  // <div class="name">You</div>
-  // <div class="text">${message.text}</div>
-  //       </div>
-  //       `;
-  //       messageContainer.appendChild(el);
-  //     } else if (type === "other") {
-  //       let el = document.createElement("div");
-  //       el.setAttribute("class", "message other-message");
-  //       el.innerHTML = `
-  //       <div>
-  // <div class="name">${message.username}</div>
-  // <div class="text">${message.text}</div>
-  //       </div>
-  //       `;
-  //       messageContainer.appendChild(el);
-  //     } else if (type === "update") {
-  //       let list = document.querySelector(".chat-screen .connected");
-  //       let el = document.createElement("div");
-  //       el.setAttribute("class", "update");
-  //       el.innerText = message;
-  //       list.appendChild(el);
-  //     }
-  //     //scroll chat to end
-  //     messageContainer.scrollTop =
-  //       messageContainer.scrollHeight - messageContainer.clientHeight;
-  //   }
 })();
